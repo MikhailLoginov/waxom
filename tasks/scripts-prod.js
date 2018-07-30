c2090fa1c671b6595c7bc8d7d244ed64a11cfcc4
Shadow <madion86@yandex.ru>
2018-07-30 03:19:59 +0300

    prod script fixed

--- tasks/scripts-prod.js
+++ tasks/scripts-prod.js
@@ -10,7 +10,7 @@ module.exports = function(options) {
       //}))
       .pipe($.gp.concat('temp.js'))
       .pipe($.gp.rename('build.js'))
-      .pipe($.gp.uglify())
+      //.pipe($.gp.uglify())
       .pipe($.gulp.dest('./dist/assets/scripts'))
   };
 };
