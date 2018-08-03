180e37b74173fcfca93078c140f08d49bf94d034
Mikhail <madion86@yandex.ru>
2018-08-03 19:03:49 +0300

    js scripts for projects and many small improvements

---
+++ src/templates/blocks/projects/projects.js
@@ -0,0 +1,57 @@
+function industrySelection() {
+  const all = [0, 1, 2, 3, 4, 5],
+    webDesign = [1, 4, 5],
+    mobileApp = [0],
+    illustration = [0, 2, 3],
+    photography = [0, 2, 3, 5];
+
+  let industryName = all;
+
+  function setActiveIndustry() {
+    Array.from(document.getElementsByClassName("projects__industry")).forEach((industry, number) => {
+      industry.addEventListener('click', () => {
+        Array.from(document.getElementsByClassName("projects__industry")).forEach((industry) => {
+          industry.className = "projects__industry";
+        });
+        industry.className += ' active';
+        switch (number) {
+          case 0:
+            industryName = all;
+            break;
+          case 1:
+            industryName = webDesign;
+            break;
+          case 2:
+            industryName = mobileApp;
+            break;
+          case 3:
+            industryName = illustration;
+            break;
+          case 4:
+            industryName = photography;
+            break;
+        };
+        displayProjects(industryName);
+        console.log(number);
+        console.log(industryName);
+      })
+    })
+  }
+
+  setActiveIndustry();
+
+  function displayProjects(industryName) {
+    Array.from(document.getElementsByClassName("project")).forEach((project, number) => {
+      if (industryName.indexOf(number) == -1) {
+        project.style.display = 'none';
+      } else {
+        project.style.display = 'flex';
+      }
+    })
+  }
+
+}
+
+$(document).ready(function () {
+  industrySelection();
+});
